a41fd41a78d72dbc13071e0502dba68a0d0990ea
Kim Eirik Kvassheim <kek91@users.noreply.github.com>
2020-10-23 15:25:44 +0200

    Legg til NavBar component

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand" href="#">Otrum &raquo; Adressbook</a>
+            
+        </nav>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavBar',
+}
+</script>
