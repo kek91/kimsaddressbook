18504eac32586b04bef5621466c2b5be61a8bf94
Kim Eirik Kvassheim <kek91@users.noreply.github.com>
2020-10-25 14:20:23 +0100

    Legg til CrossOrigin for å unngå CORS error på localhost:8081

--- backend/src/main/java/com/example/accessingdatarest/PersonRepository.java
+++ backend/src/main/java/com/example/accessingdatarest/PersonRepository.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+// @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@CrossOrigin(origins = "http://localhost:8081")
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
 
-	// List<Person> findByName(@Param("name") String name);
-	// List<Person> findByAge(@Param("age") int age);
 	// List<Person> findByLastName(@Param("name") String name);
-	// List<Person> searchByName(@Param("name") String name);
-
+	
 }
