ee4af68e1c01ed33d2b811561feff4cde7f2b5bc
Kim Eirik Kvassheim <kek91@users.noreply.github.com>
2020-10-27 01:21:33 +0100

    Legg inn NavBar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,8 +2,20 @@
     <div>
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="#">Otrum &raquo; Adressbook</a>
-            
+            <a class="navbar-brand" href="#">KimsAddressbook</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    
+                </ul>
+                <span class="navbar-text">
+                    <!-- <button class="btn btn-sm btn-outline-light mx-2" type="button" @click="setTheme('light')">Lys</button> -->
+                    <!-- <button class="btn btn-sm btn-outline-secondary mx-2" type="button" @click="setTheme('dark')">Mørk</button> -->
+                    <button class="btn btn-sm btn-outline-warning mx-2 px-2" type="button" @click="setTheme('hehe')">😃</button>
+                </span>
+            </div>
         </nav>
 
     </div>
